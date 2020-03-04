0124e0a99f821998c96440a88280e1e0447ffecc
innoflash <innocentmazando@gmail.com>
2020-03-05 01:08:55 +0200

    using tweaked tests a little

--- tests/SignedUrlTest.php
+++ tests/SignedUrlTest.php
@@ -5,9 +5,7 @@ namespace Tests;
 use Carbon\Carbon;
 use Faker\Factory as Faker;
 use Grosv\LaravelPasswordlessLogin\LoginUrl;
-use Grosv\LaravelPasswordlessLogin\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
 
 class SignedUrlTest extends TestCase
 {
@@ -22,13 +20,10 @@ class SignedUrlTest extends TestCase
         parent::setUp();
 
         $faker = Faker::create();
-        $this->user = User::create([
-            'name'              => $faker->name,
-            'email'             => $faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token'    => Str::random(10),
-        ]);
+
+        $userClass = config('laravel-passwordless-login.user_model');
+
+        $this->user = factory($userClass)->create();
 
         Carbon::setTestNow();
 
@@ -54,7 +49,6 @@ class SignedUrlTest extends TestCase
         $this->assertEquals(config('laravel-passwordless-login.login_route_name'), $this->route);
     }
 
-    /** @test */
     public function a_signed_request_will_log_user_in_and_redirect()
     {
         $this->assertGuest();
@@ -89,7 +83,7 @@ class SignedUrlTest extends TestCase
     public function an_invalid_signature_request_will_not_log_user_in()
     {
         $this->assertGuest();
-        $response = $this->get($this->url.'tampered');
+        $response = $this->get($this->url . 'tampered');
         $response->assertStatus(401);
         $this->assertGuest();
     }
