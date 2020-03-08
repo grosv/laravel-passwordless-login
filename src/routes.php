2e6c80bb79a9b8691756bb0224f7479d584a4937
innoflash <innocentmazando@gmail.com>
2020-03-08 18:34:49 +0200

    refactored route actions to a controller

--- src/routes.php
+++ src/routes.php
@@ -1,20 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Grosv\LaravelPasswordlessLogin\LaravelPasswordlessLoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get(config('laravel-passwordless-login.login_route').'/{expires}/{uid}', function (Request $request) {
-    abort_if(!$request->hasValidSignature(), 401);
+Route::get(
+    config('laravel-passwordless-login.login_route').'/{expires}/{uid}',
+    [LaravelPasswordlessLoginController::class, 'login']
+)
+    ->name(config('laravel-passwordless-login.login_route_name'));
 
-    $user_model = config('laravel-passwordless-login.user_model');
-
-    Auth::guard(config('laravel-passwordless-login.user_guard'))
-        ->login($user_model::find($request->uid), 'laravel-passwordless-login.remember_login');
-
-    redirect(config('laravel-passwordless-login.redirect_on_success'));
-})->name(config('laravel-passwordless-login.login_route_name'));
-
-Route::get('/laravel_passwordless_login_redirect_test_route', function (Request $request) {
-    return response()->noContent(204);
-});
+Route::get('/laravel_passwordless_login_redirect_test_route', [LaravelPasswordlessLoginController::class, 'redirectTestRoute']);
