11290b9171fcb21a07866ccee7590972ea468333
Benjamin Combe <aldea@arthion.fr>
2020-04-21 21:05:35 +0200

    #30 Fix link never expiring : remove expires from the path to implicitly set is as a parameter

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,7 @@ use Grosv\LaravelPasswordlessLogin\LaravelPasswordlessLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get(
-    config('laravel-passwordless-login.login_route').'/{expires}/{uid}',
+    config('laravel-passwordless-login.login_route').'/{uid}',
     [LaravelPasswordlessLoginController::class, 'login']
 )->middleware('web')->name(config('laravel-passwordless-login.login_route_name'));
 
