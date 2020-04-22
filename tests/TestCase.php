d01dfcc9049daad855ec69506f658ddc5964d448
Benjamin Combe <aldea@arthion.fr>
2020-04-22 09:54:47 +0200

    StyleCi update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -38,6 +38,5 @@ abstract class TestCase extends BaseTestCase
     {
         $app['config']->set('database.default', 'testing');
         $app['config']->set('app.key', 'base64:r0w0xC+mYYqjbZhHZ3uk1oH63VadA3RKrMW52OlIDzI=');
-        
     }
 }
