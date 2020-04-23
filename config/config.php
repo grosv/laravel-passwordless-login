53b1a360fa6c253615caceb0b9f7c79284a83ddb
kamiben <paul_atreide_dune@hotmail.com>
2020-04-23 09:22:55 +0200

    Add the possibility to customize the message laravel returns when the signature is expired or invalid

--- config/config.php
+++ config/config.php
@@ -8,4 +8,5 @@ return[
     'login_route_name'    => env('LPL_LOGIN_ROUTE_NAME', 'magic-login'),
     'login_route_expires' => env('LPL_LOGIN_ROUTE_EXPIRES', '30'),
     'redirect_on_success' => env('LPL_REDIRECT_ON_LOGIN', '/'),
+    'invalid_signature_message' => env('LPL_INVALID_SIGNATURE_MESSAGE', ''),
 ];
