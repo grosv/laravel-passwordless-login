ffaf7ab4ee5735c3d54b440fb0d92b33f922eef4
kamiben <paul_atreide_dune@hotmail.com>
2020-04-23 09:42:59 +0200

    The tests failed as the defaut message cannot be null

--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 return [
     'user_model'          => env('LPL_USER_MODEL', 'App\User'),
     'user_guard'          => env('LPL_USER_GUARD', 'web'),
@@ -11,4 +12,15 @@ return [
     'login_use_once'      => false,
     'invalid_signature_message' => env('LPL_INVALID_SIGNATURE_MESSAGE', ''),
 
+=======
+return[
+    'user_model'                => env('LPL_USER_MODEL', 'App\User'),
+    'user_guard'                => env('LPL_USER_GUARD', 'web'),
+    'remember_login'            => env('LPL_REMEMBER_LOGIN', false),
+    'login_route'               => env('LPL_LOGIN_ROUTE', '/magic-login'),
+    'login_route_name'          => env('LPL_LOGIN_ROUTE_NAME', 'magic-login'),
+    'login_route_expires'       => env('LPL_LOGIN_ROUTE_EXPIRES', '30'),
+    'redirect_on_success'       => env('LPL_REDIRECT_ON_LOGIN', '/'),
+    'invalid_signature_message' => env('LPL_INVALID_SIGNATURE_MESSAGE', 'Invalid or Expired link'),
+>>>>>>> The tests failed as the defaut message cannot be null
 ];
