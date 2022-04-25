d0e7b7b5167a2b7d6e7245e6230fce477690c412
Reinis Lucis <reinis@onthemapmarketing.com>
2022-04-25 08:54:47 +0300

    Move middleware to config

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get(
     config('laravel-passwordless-login.login_route').'/{uid}',
     [LaravelPasswordlessLoginController::class, 'login']
-)->middleware(['web', HandleAuthenticatedUsers::class])->name(config('laravel-passwordless-login.login_route_name'));
+)->middleware(config('laravel-passwordless-login.middleware', ['web', HandleAuthenticatedUsers::class])->name(config('laravel-passwordless-login.login_route_name'));
 
 Route::get('/laravel_passwordless_login_redirect_test_route', [LaravelPasswordlessLoginController::class, 'redirectTestRoute'])->middleware('auth');
 Route::get('/laravel_passwordless_login_redirect_overridden_route', [LaravelPasswordlessLoginController::class, 'redirectTestRoute'])->middleware('auth');
