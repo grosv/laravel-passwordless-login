4603cc1ee3aab16b2d9669c6d0da45689cc8e3b6
Joe <joe@joe-pritchard.uk>
2022-04-28 15:17:41 +0100

    Update HandleAuthenticatedUsers.php
    When finding the home route to send authenticated users to, handle cases where the HOME constant is not set on the RouteServiceProvider, the home() method doesn't exist, or the app's namespace has been changed

--- src/HandleAuthenticatedUsers.php
+++ src/HandleAuthenticatedUsers.php
@@ -13,14 +13,34 @@ class HandleAuthenticatedUsers
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                $home = class_exists(\App\Providers\RouteServiceProvider::class)
-                    ? \App\Providers\RouteServiceProvider::HOME
-                    : config('laravel-passwordless-login.redirect_on_success', '/');
 
-                return redirect($request->get('redirect_to', $home));
+                return redirect($request->get('redirect_to', self::getHomeRoute()));
             }
         }
 
         return $next($request);
+    }
+
+    /**
+     * Figure out the home route the programmer has defined in their RouteServiceProvider (if it exists) by checking the
+     * home() function, then the HOME constant, then finally the value from the config file
+     *
+     * @static function getHomeRoute
+     */
+    private static function getHomeRoute(): string
+    {
+        $provider_name = '\\' . app()->getNamespace() . 'Providers\\RouteServiceProvider';
+
+        if (class_exists($provider_name)) {
+            if (method_exists($provider_name, 'home')) {
+                return call_user_func([$provider_name, 'home']);
+            }
+
+            if (defined($provider_name . '::HOME')) {
+                return constant($provider_name . '::HOME');
+            }
+        }
+
+        return config('laravel-passwordless-login.redirect_on_success', '/');
     }
 }
